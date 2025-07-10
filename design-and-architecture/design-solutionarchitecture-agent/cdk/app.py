55835165795d66a3ccb6ba2f3b635b94fa25af9e
hknyl <hknyldrm@amazon.com>
2025-07-10 11:37:40 +0200

    Update app.py

--- design-and-architecture/design-solutionarchitecture-agent/cdk/app.py
+++ design-and-architecture/design-solutionarchitecture-agent/cdk/app.py
@@ -2,15 +2,11 @@
 import os
 
 import aws_cdk as cdk
-import cdk_nag
-
 from stack.stack import AWSAgentStack
 
 app = cdk.App()
 AWSAgentStack(app, "AWSAgentStack",
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 )
-
-cdk.Aspects.of(app).add(cdk_nag.AwsSolutionsChecks(verbose=True))
 
 app.synth()
